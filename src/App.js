7d7439da8933e8ca0a7f21779324a6270f1ae44c
Clarissa Diesinger <clarissa.diesinger@outlook.com>
2020-04-22 08:09:46 +0200

    added component ListContact

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
+
+const contacts = [
+  {
+    id: 'karen',
+    name: 'Karen Isgrigg',
+    handle: 'karen_isgrigg',
+    avatarURL: 'http://localhost:5001/karen.jpg',
+  },
+  {
+    id: 'richard',
+    name: 'Richard Kalehoff',
+    handle: 'richardkalehoff',
+    avatarURL: 'http://localhost:5001/richard.jpg',
+  },
+  {
+    id: 'tyler',
+    name: 'Tyler McGinnis',
+    handle: 'tylermcginnis',
+    avatarURL: 'http://localhost:5001/tyler.jpg',
+  },
+]
 
 class App extends Component {
   render() {
+    // In order to get data to a Component you pass that
+    // data in as a prop when you create the Component element
     return (
       <div>
-        Hello World
+        <ListContacts contacts={contacts} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
