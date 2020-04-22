f7fd02aacec1f8f656c6aa3aa1aa4d036a6fdb16
Clarissa Diesinger <clarissa.diesinger@outlook.com>
2020-04-22 16:02:12 +0200

    Added contacts to list.

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -4,7 +4,13 @@ class ListContacts extends Component {
   // render method describes UI of our component
   render() {
     console.log('Props', this.props)
-    return <ol className="contact-list" />
+    return (
+      <ol className="contact-list">
+        {this.props.contacts.map((contact) => (
+          <li key={contact.id}>{contact.name}</li>
+        ))}
+      </ol>
+    )
   }
 }
 
